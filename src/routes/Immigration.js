b17246c943874c865e531e21bcfb479739ddff0b
rathorexccelerata <rathore.ishan@gmail.com>
2022-07-08 12:51:36 -0400

    Initial Commit

--- src/routes/Immigration.js
+++ src/routes/Immigration.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import FooterDetails from '../components/FooterDetails'
+import HeaderDetails from '../components/HeaderDetails'
 import ImmigrationDetails from '../components/ImmigrationDetails'
 
 const Immigration = () => {
   return (
     <>
+    <HeaderDetails/>
     <ImmigrationDetails/>
     <FooterDetails/>
     </>
